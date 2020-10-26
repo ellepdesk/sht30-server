ee23db10020442ca890b6a56d370ee70eb1006ba
Pelle van der Heide <P.S.vanderHeide@gmail.com>
2020-10-26 16:16:32 +0100

    Use id to select fields to update in html/js

--- data/www/local.js
+++ data/www/local.js
@@ -5,7 +5,7 @@
         url:"rt", 
         success: function(data) {
             $.each(data, function(k, v){
-                $('.'+k).text(v);
+                $('#'+k).text(v);
             })},
         complete: function() {
         // Schedule the next request when the current one's complete
